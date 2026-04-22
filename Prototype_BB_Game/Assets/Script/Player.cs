1f194ae9efaba71f6315cdc3b21bc50674085d8a
Eriko Kresna <56141967+erikokres@users.noreply.github.com>
2026-04-22 15:56:33 +0700

    add Multiplayer

--- Prototype_BB_Game/Assets/Script/Player.cs
+++ Prototype_BB_Game/Assets/Script/Player.cs
@@ -1,22 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Drawing;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Mirror;
+using UnityEngine.VFX;
 
-public class Player : MonoBehaviour
+public class Player : NetworkBehaviour
 {
 
     private Vector2 movementInput, pointerInput;
     private Rigidbody2D rb;
     private bool FacingRight = true;
+    private PlayerInput inputAction;
+    private float lastDashTime;
 
     public Vector2 PointerInput => pointerInput;
 
     [SerializeField]
     private InputActionReference movement, shoot, pointerPositions;
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float dashForce = 10f;
+    [SerializeField] private float dashCooldown = 1f;
 
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -26,29 +32,54 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        pointerInput = GetPointerInput();
-
-        movementInput = movement.action.ReadValue<Vector2>();
-
-        rb.velocity = movementInput * moveSpeed;
-
-        //Flip Character
+        if (isLocalPlayer)
+        {
+            OnWalk();
+            if (shoot.action.triggered)
+            {
+                Dash();
+                
+            }
+        }
         
     }
+
+    private void OnWalk()
+    {
+        movementInput = movement.action.ReadValue<Vector2>();
+        rb.velocity = movementInput * moveSpeed;
+    }
+    private void Dash()
+    {
+        if(Time.time < lastDashTime + dashCooldown) return;
+
+        lastDashTime = Time.time;
+
+        Vector2 dashDirection = movementInput;
+        
+        if(dashDirection == Vector2.zero)
+        {
+            dashDirection = Vector2.up;
+        }
+
+        CmdDash(dashDirection);
+    }
+
+    [Command]
+    void CmdDash(Vector2 direction)
+    {
+        rb.AddForce(direction * dashForce, ForceMode2D.Impulse);
+        Debug.Log("Dash");
+    }
+    
+
 
     private Vector2 GetPointerInput()
     {
         Vector3 mousePos = pointerPositions.action.ReadValue<Vector2>();
         mousePos.z = Camera.main.nearClipPlane;
         return Camera.main.ScreenToWorldPoint(mousePos);
-        if (mousePos.x < transform.position.x && FacingRight)
-        {
-            Flip();
-        }
-        else if(mousePos.x > transform.position.x && FacingRight)
-        {
-            Flip();
-        }
+        
     }
 
     public void Flip()
